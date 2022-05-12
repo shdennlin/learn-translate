f68e0e41c39b4badba73dc7a03c32c308ca0db61
shdennlin <shawndennislin@gmail.com>
2022-05-12 20:40:56 +0800

    [feat] add google_dict_api

--- README.md
+++ README.md
@@ -1,6 +1,19 @@
 # Learn Translate
+
 Learn Translate is a simple and lightweight translator written in Python / Qt that allows you to translate and speak text using Google translate API.
 
 
-# Ref
-1. ECDICT: https://github.com/skywind3000/ECDICT
+## Related Projects
+
+### [ECDICT](https://github.com/skywind3000/ECDICT)
+
+Free English to Chinese Dictionary Database.
+
+### [Free Dictionary API](https://github.com/meetDeveloper/freeDictionaryAPI) 
+
+There was no free Dictionary API on the web when I wanted one for my friend, so I created one.
+
+## API
+
+1. [Cloud Translation API](https://cloud.google.com/translate) 
+2. [Cloud Text-to-Speech API](https://cloud.google.com/text-to-speech)
--- src/api.py
+++ src/api.py
@@ -8,18 +8,18 @@ from .setting import get_settings
 
 
 @logger.catch
-def download_audio(text="",
+def download_audio(word="",
                    languageCode="en-US",
                    audio_path=Path(get_settings().TMP_DIR, "temp.mp3")):
     key = get_settings().TEXT_TO_SPEECH_API_KEY
-    url = "https://texttospeech.googleapis.com/v1/text:synthesize"
+    url = "https://texttospeech.googleapis.com/v1/word:synthesize"
     params = {"key": key}
     json = {
         "voice": {
             "languageCode": languageCode
         },
         "input": {
-            "text": text
+            "word": word
         },
         "audioConfig": {
             "audioEncoding": "mp3"
@@ -37,17 +37,18 @@ def download_audio(text="",
 
 
 @logger.catch
-def translate_text_api(text="", target="zh_TW"):
+def translate_text_api(word="", target="zh_TW"):
+    """ return response.json()"""
 
     key = get_settings().TRANSLATION_API_KEY
     url = "https://translation.googleapis.com/language/translate/v2"
-    params = {"key": key, "target": target, "q": text}
+    params = {"key": key, "target": target, "q": word}
 
     response = requests.post(url, params=params)
     logger.debug(f"cloud translation response = {response.status_code}")
     if response.status_code == 200:
         response = response.json()["data"]["translations"][0]["translatedText"]
-        logger.debug(f"cloud translation text = {response}")
+        logger.debug(f"cloud translation word = {response}")
         return response
     else:
         logger.error(
@@ -56,17 +57,55 @@ def translate_text_api(text="", target="zh_TW"):
 
 
 @logger.catch
-def english_dict_api(text=""):
+def eng_eng_dict_api(word=""):
+    """ return response.json()"""
 
-    url = "https://api.dictionaryapi.dev/api/v2/entries/en/" + text
+    url = "https://api.dictionaryapi.dev/api/v2/entries/en/" + word
 
     response = requests.get(url)
-    logger.debug(f"English Dictionary response = {response.status_code}")
+    logger.debug(f"English Dictionary API = {response.status_code}")
     if response.status_code == 200:
         response = response.json()
-        logger.debug(f"English Dictionary text = {response}")
+        logger.debug(
+            f"English Dictionary API, word={word}, response={response}")
         return response
+    elif response.status_code == 404:
+        return None
     else:
         logger.error(
             f"cloud translation response error, status_code = {response.status_code}"
+        )
+
+
+@logger.catch
+def google_dict_api(word="test",
+                    source_lanuage="auto",
+                    target_language="zh_TW",
+                    ui_language="en"):
+    """ return response.json()"""
+
+    url = "https://translate.googleapis.com/translate_a/single?client=gtx&ie=UTF-8&oe=UTF-8&dt=bd&dt=ex&dt=ld&dt=md&dt=rw&dt=rm&dt=ss&dt=t&dt=at&dt=qc"
+
+    params = {
+        "client": "gtx",
+        "ie": "UTF-8",
+        "oe": "UTF-8",
+        "dt": ["bd", "ex", "ld", "md", "rw", "rm", "ss", "t", "at", "qc"],
+        "sl": source_lanuage,
+        "tl": target_language,
+        "hl": ui_language,
+        "q": word
+    }
+
+    response = requests.get(url, params=params)
+    logger.debug(f"Google free Dict API = {response.status_code}")
+    if response.status_code == 200:
+        response = response.json()
+        logger.debug(f"Google free Dict API, word={word}, response={response}")
+        return response
+    elif response.status_code == 404:
+        return None
+    else:
+        logger.error(
+            f"Google free Dict API response error, status_code = {response.status_code}"
         )
