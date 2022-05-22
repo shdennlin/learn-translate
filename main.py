4e6427dbfc958f8a5ae1e26032733f979dac0810
shdennlin <shawndennislin@gmail.com>
2022-05-22 10:55:40 +0800

    [refactor] complete model

--- main.py
+++ main.py
@@ -2,47 +2,66 @@
 from pygame import mixer
 from pathlib import Path
 import time
+import codecs
+import json
 
-from src.setting import get_settings, GlobalVariable
-from src.api import download_audio, translate_text_api, english_dict_api
-from src.audio import play_audio, stop_audio
-import src.stardict as startdict
+from src.models.ApiModel import *
+from src.models.AudioModel import *
+# from src.setting import get_settings, GlobalVariable
+# from src.api import download_audio, translate_text_api, eng_eng_dict_api, google_dict_api
+# from src.audio import play_audio, stop_audio
+# from src.get_result import get_word_means
+# from src.stardict import StarDict
+# from src.usecases import print_response_json, write_response_json
 
-if __name__ == '__main__':
+
+def main():
     g = GlobalVariable()
 
+    word = "inhabit"
+    # get_word_means(word, eng_chi_dict_api)
+
+    word = "inhabit"
+    s = time.time()
+    #=====ECDICT==========================================================
     stardict_db_path = Path(get_settings().BASE_DIR, "data", "stardict.db")
-    engdict = startdict.StarDict(stardict_db_path)
-    # res_match = engdict.match("gather up")
-    # res = engdict.query(res_match[0][1])
+    eng_chi_dict_api = StarDict(stardict_db_path)
+    # res_match = eng_chi_dict_api.match("gather up")
+    # res = eng_chi_dict_api.query(res_match[0][1])
+    res = eng_chi_dict_api.query(word)
+    write_response_json(res, Path(get_settings().TMP_DIR, "ECEICT.json"))
 
-    #==================================================================
-    # English Dict
-    text = "arguments"
-    result = english_dict_api(text)
-    print(result)
+    # =====Cloud Translate API============================================
+    res = translate_text_api(["bitch", "process"])
+    if res:
+        print(res["data"]["translations"])
 
-    #==================================================================
-    # Query
-    # text = "ASAP"
-    # res = engdict.query(text)
-    # if res == None:
-    #     res = translate_text_api(text)
+    # =====Free Dictionary API============================================
+    res = eng_eng_dict_api(word)
+    # print_response_json(res)
+    write_response_json(res,
+                        Path(get_settings().TMP_DIR, "FreeDictionaryAPI.json"))
 
-    # print(res)
+    # =====Google Dictionary API==========================================
+    res = google_dict_api(word)
+    # print_response_json(res)
+    write_response_json(
+        res, Path(get_settings().TMP_DIR, "GoogleDictionaryAPI.json"))
 
-    #==================================================================
-    # translate
-    # text = "The arguments shown above are merely"
-    # result = translate_text_api(res["translation"])
-    # print(result)
+    # =====play audio=====================================================
+    mixer.init()
+    download_audio(word)
+    e = time.time()
+    print(e - s)
 
-    #==================================================================
-    # play audio
-    # mixer.init()
-    # download_audio(text)
-    # audio_path = Path(get_settings().TMP_DIR, "temp.mp3")
-    # play_audio(g, mixer.music, audio_path)
+    audio_path = Path(get_settings().TMP_DIR, "temp.mp3")
+    play_audio(g, mixer.music, audio_path)
 
-    # while mixer.music.get_busy():
-    #     time.sleep(0.1)
+    while mixer.music.get_busy():
+        time.sleep(0.1)
+
+
+if __name__ == '__main__':
+    res = translate_text_api(["bitch", "process"])
+    if res:
+        print(res["data"]["translations"])
--- src/models/ApiModel.py
+++ src/models/ApiModel.py
@@ -1,86 +1,84 @@
 import base64
 import os
-import requests
 from loguru import logger
 from pathlib import Path
 
-from .setting import get_settings
-from .usecases import requests_funs
+from ..usecases.usecases import requests_funs
+from ..usecases.setting import get_settings
 
 
-@logger.catch
-def download_audio(text="",
-                   languageCode="en-US",
-                   audio_path=Path(get_settings().TMP_DIR, "temp.mp3")):
-    key = get_settings().TEXT_TO_SPEECH_API_KEY
-    url = "https://texttospeech.googleapis.com/v1/text:synthesize"
-    params = {"key": key}
-    json = {
-        "voice": {
-            "languageCode": languageCode
-        },
-        "input": {
-            "text": text
-        },
-        "audioConfig": {
-            "audioEncoding": "mp3"
+class ApiModel():
+
+    @logger.catch
+    def download_audio(text="",
+                       languageCode="en-US",
+                       audio_path=Path(get_settings().TMP_DIR, "temp.mp3")):
+        key = get_settings().TEXT_TO_SPEECH_API_KEY
+        url = "https://texttospeech.googleapis.com/v1/text:synthesize"
+        params = {"key": key}
+        json = {
+            "voice": {
+                "languageCode": languageCode
+            },
+            "input": {
+                "text": text
+            },
+            "audioConfig": {
+                "audioEncoding": "mp3"
+            }
         }
-    }
 
-    response = requests_funs("Cloud Text to Speech API", "post", url, params,
-                             json)
-    try:
-        os.remove(audio_path)
-    except:
-        pass
+        response = requests_funs("Cloud Text to Speech API", "post", url,
+                                 params, json)
+        try:
+            os.remove(audio_path)
+        except:
+            pass
 
-    if response:
-        with open(audio_path, 'wb') as fp:
-            fp.write(base64.b64decode(response["audioContent"]))
+        if response:
+            with open(audio_path, 'wb') as fp:
+                fp.write(base64.b64decode(response["audioContent"]))
 
+    @logger.catch
+    def translate_text_api(word="", target="zh_TW"):
+        """ return response.json()"""
 
-@logger.catch
-def translate_text_api(word="", target="zh_TW"):
-    """ return response.json()"""
+        key = get_settings().TRANSLATION_API_KEY
+        url = "https://translation.googleapis.com/language/translate/v2"
+        params = {"key": key, "target": target, "q": word}
 
-    key = get_settings().TRANSLATION_API_KEY
-    url = "https://translation.googleapis.com/language/translate/v2"
-    params = {"key": key, "target": target, "q": word}
+        response = requests_funs("Cloud Translate API", "post", url, params)
 
-    response = requests_funs("Cloud Translate API", "post", url, params)
+        return response
 
-    return response
+    @logger.catch
+    def eng_eng_dict_api(word=""):
+        """ return response.json()"""
 
+        url = "https://api.dictionaryapi.dev/api/v2/entries/en/" + word
+        response = requests_funs("Free Dictionary API", "get", url)
+        return response
 
-@logger.catch
-def eng_eng_dict_api(word=""):
-    """ return response.json()"""
+    @logger.catch
+    def google_dict_api(word="test",
+                        source_lanuage="auto",
+                        target_language="zh_TW",
+                        ui_language="en"):
+        """ return response.json()"""
 
-    url = "https://api.dictionaryapi.dev/api/v2/entries/en/" + word
-    response = requests_funs("Free Dictionary API", "get", url)
-    return response
+        url = "https://translate.googleapis.com/translate_a/single?client=gtx&ie=UTF-8&oe=UTF-8&dt=bd&dt=ex&dt=ld&dt=md&dt=rw&dt=rm&dt=ss&dt=t&dt=at&dt=qc"
 
+        params = {
+            "client": "gtx",
+            "ie": "UTF-8",
+            "oe": "UTF-8",
+            "dt": ["bd", "ex", "ld", "md", "rw", "rm", "ss", "t", "at", "qc"],
+            "sl": source_lanuage,
+            "tl": target_language,
+            "hl": ui_language,
+            "q": word
+        }
 
-@logger.catch
-def google_dict_api(word="test",
-                    source_lanuage="auto",
-                    target_language="zh_TW",
-                    ui_language="en"):
-    """ return response.json()"""
-
-    url = "https://translate.googleapis.com/translate_a/single?client=gtx&ie=UTF-8&oe=UTF-8&dt=bd&dt=ex&dt=ld&dt=md&dt=rw&dt=rm&dt=ss&dt=t&dt=at&dt=qc"
-
-    params = {
-        "client": "gtx",
-        "ie": "UTF-8",
-        "oe": "UTF-8",
-        "dt": ["bd", "ex", "ld", "md", "rw", "rm", "ss", "t", "at", "qc"],
-        "sl": source_lanuage,
-        "tl": target_language,
-        "hl": ui_language,
-        "q": word
-    }
-
-    response = requests_funs("Google free Dictionary API", "get", url, params)
-    # response = requests.get(url, params=params)
-    return response
+        response = requests_funs("Google free Dictionary API", "get", url,
+                                 params)
+        return response
